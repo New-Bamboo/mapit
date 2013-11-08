344b78b4a9d7deb3de3509ab18bc76d3f5003670
Iain Beeston <iain.beeston@gmail.com>
2013-11-08 13:44:29 +0000

    Configured static assets as recommended by heroku

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,4 +8,5 @@ sys.path.insert(0, os.path.normpath(os.path.join(file_dir, '..')))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
